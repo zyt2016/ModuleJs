fd85248ae6fdef243734fc34d42c6a247fc8b43e
zyt2016 <672445427@qq.com>
2016-09-09 10:18:23 +0800

    Update 判断只能数字.js

--- 判断只能数字.js
+++ 判断只能数字.js
@@ -1,1 +1,25 @@
 
+#dairyNotes
+#判断某个控件的值必须为数字(jq)
+
+<script type = "text/javascript">
+   /**
+   * @param  {控件的val值}
+   * @return {Boolean}
+   */
+  function isNum(val) {
+      var flag = true; //控制返回值
+      var reg = /\d/gi; //正则表达式
+      var len = 0;
+      val = $.trim(val); //滤掉值两边的空格
+      if (val.match(reg) != null) {
+          len = val.match(reg).length;
+      }
+
+      if (len != val.length || len === 0) {
+          flag = false;
+      }
+      return flag;
+  }
+  
+</script>
